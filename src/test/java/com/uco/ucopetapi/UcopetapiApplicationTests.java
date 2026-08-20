b5449761ff078962555146e4b2f67de99ceea843
Cristopher Ospina <180292639+cristopherr26@users.noreply.github.com>
2026-08-20 09:16:25 -0500

    Commit inicial

---
+++ src/test/java/com/uco/ucopetapi/UcopetapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uco.ucopetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcopetapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
