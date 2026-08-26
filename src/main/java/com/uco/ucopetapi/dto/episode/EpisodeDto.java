ebf84db26891f349577c69b56d2b049a6ac35e3d
Dany Alexander Cardona Gómez <Danyx3k@gmail.com>
2026-08-26 14:30:01 -0500

    Create PetCare DTO and update Episode DTO

--- src/main/java/com/uco/ucopetapi/dto/episode/EpisodeDto.java
+++ src/main/java/com/uco/ucopetapi/dto/episode/EpisodeDto.java
@@ -1,8 +1,57 @@
 package com.uco.ucopetapi.dto.episode;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 public class EpisodeDto {
-    private UUID UUID;
+    private UUID id;
     private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private boolean status;
+
+    public EpisodeDto (final UUID id,final String description,final LocalDate starDate,final LocalDate endDate, boolean status){
+        this.id = id;
+        this.description = description;
+        this.startDate = starDate;
+        this.endDate = endDate;
+        this.status=status;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
--- src/main/java/com/uco/ucopetapi/dto/petCare/PetCareDto.java
+++ src/main/java/com/uco/ucopetapi/dto/petCare/PetCareDto.java
@@ -1,4 +1,71 @@
 package com.uco.ucopetapi.dto.petCare;
 
+import java.time.LocalDate;
+
+import java.util.UUID;
+
 public class PetCareDto {
+    private UUID id;
+    private UUID petsDto;
+    private UUID episodeDTO;
+    private UUID procedureDTO;
+    private UUID productDTO;
+    private LocalDate attentionDate;
+    private String description;
+    private boolean petCateStatus;
+
+    public PetCareDto (UUID id, UUID petsDto, UUID episodeDto, UUID procedureDto, UUID productDto, LocalDate attentionDate, String description, boolean petCareStatus){
+        this.id = id;
+        this.petsDto = petsDto;
+        this.episodeDTO = episodeDto;
+        this.procedureDTO = procedureDto;
+        this.productDTO = productDto;
+        this.attentionDate = attentionDate;
+        this.description = description;
+        this.petCateStatus= petCareStatus;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public UUID getPetsDto() {
+        return petsDto;
+    }
+
+    public UUID getEpisodeDTO() {
+        return episodeDTO;
+    }
+
+    public UUID getProcedureDTO() {
+        return procedureDTO;
+    }
+
+    public UUID getProductDTO() {
+        return productDTO;
+    }
+
+    public LocalDate getAttentionDate() {
+        return attentionDate;
+    }
+
+    public void setAttentionDate(LocalDate attentionDate) {
+        this.attentionDate = attentionDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isPetCateStatus() {
+        return petCateStatus;
+    }
+
+    public void setPetCateStatus(boolean petCateStatus) {
+        this.petCateStatus = petCateStatus;
+    }
 }
