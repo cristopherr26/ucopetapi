c546a12c71db1584ab1a0666ed9e4c710e94929b
julianamontoya <montoya.julianav@gmail.com>
2026-08-27 03:24:16 -0500

    Creación package de Controllers y DTO de HealthPlan y HealthPlanCoverage
    creación de los controllers y DTO y prueba de cada una de las request para asegurar la comunicación

--- src/main/java/com/uco/ucopetapi/dto/healthplan/HealthPlanDTO.java
+++ src/main/java/com/uco/ucopetapi/dto/healthplan/HealthPlanDTO.java
@@ -1,4 +1,73 @@
 package com.uco.ucopetapi.dto.healthplan;
 
+import com.uco.ucopetapi.dto.healthplancoverage.HealthPlanCoverageDTO;
+
+import java.util.UUID;
+import java.util.List;
+
 public class HealthPlanDTO {
+
+    private UUID id;
+    private String name;
+    private String insuranceCompany;
+    private String description;
+    private List<HealthPlanCoverageDTO> coverages;
+
+    public HealthPlanDTO() {
+    }
+
+    public HealthPlanDTO(
+            UUID id,
+            String name,
+            String insuranceCompany,
+            String description,
+            List<HealthPlanCoverageDTO> coverages) {
+
+        this.id = id;
+        this.name = name;
+        this.insuranceCompany = insuranceCompany;
+        this.description = description;
+        this.coverages = coverages;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInsuranceCompany() {
+        return insuranceCompany;
+    }
+
+    public void setInsuranceCompany(String insuranceCompany) {
+        this.insuranceCompany = insuranceCompany;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<HealthPlanCoverageDTO> getCoverages() {
+        return coverages;
+    }
+
+    public void setCoverages(List<HealthPlanCoverageDTO> coverages) {
+        this.coverages = coverages;
+    }
 }
+
