9e98d37b62b93fc2bfae0dfb9386d9c4e74cd032
Clara Rivillas <160364967+crmgg@users.noreply.github.com>
2026-08-29 11:22:59 -0500

    Segundo commit controller y dto correciones ademeas de  controller y dto de SpecialtieDoctor

--- src/main/java/com/uco/ucopetapi/controllers/specialtieDoctor/SpecialtieDoctorController.java
+++ src/main/java/com/uco/ucopetapi/controllers/specialtieDoctor/SpecialtieDoctorController.java
@@ -20,14 +20,43 @@ public class SpecialtieDoctorController {
         );
     }
 
+    @GetMapping
+    public ResponseEntity<List<SpecialtieDoctorDTO>> findAllSpecialtieDoctor() {
+        List<SpecialtieDoctorDTO> specialtieDoctor = List.of(
+                buildDummySpecialtieDoctor(UUID.randomUUID())
+        );
+
+        return ResponseEntity.ok(specialtieDoctor);
+    }
+
     @GetMapping("/{id}")
-    public ResponseEntity<List<SpecialtieDoctorDTO>> findById(@PathVariable UUID id) {
+    public ResponseEntity<SpecialtieDoctorDTO> findById(@PathVariable UUID id) {
         return ResponseEntity.ok(buildDummySpecialtieDoctor(id));
     }
 
     @GetMapping("/filter")
-    public ResponseEntity<List<SpecialtieDoctorDTO>> findByIdFilter(@RequestParam (required = false) UUID id){
+    public ResponseEntity<List<SpecialtieDoctorDTO>> findByIdFilter(@RequestParam(required = false) UUID id) {
         return ResponseEntity.ok(List.of(buildDummySpecialtieDoctor(id)));
     }
 
-    
+    @PostMapping
+    public ResponseEntity<SpecialtieDoctorDTO> createNewSpecialtieDoctor(@RequestBody SpecialtieDoctorDTO specialtieDoctor) {
+        SpecialtieDoctorDTO createdSpecialtieDoctor = buildDummySpecialtieDoctor(specialtieDoctor.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdSpecialtieDoctor);
+    }
+
+    @PutMapping
+    public ResponseEntity<SpecialtieDoctorDTO> updateSpecialtieDoctor(
+            @RequestParam(required = true) UUID id,
+            @RequestBody SpecialtieDoctorDTO specialtieDoctor) {
+
+        SpecialtieDoctorDTO updatedSpecialtieDoctor = buildDummySpecialtieDoctor(id);
+        return ResponseEntity.ok(updatedSpecialtieDoctor);
+    }
+
+    @PutMapping("/deactivate")
+    public ResponseEntity<Void> deactivateSpecialtieDoctor(@RequestParam(required = true) UUID id) {
+        return ResponseEntity.noContent().build();
+    }
+
+}
--- src/main/java/com/uco/ucopetapi/dto/specialtieDoctor/SpecialtieDoctorDTO.java
+++ src/main/java/com/uco/ucopetapi/dto/specialtieDoctor/SpecialtieDoctorDTO.java
@@ -1,4 +1,44 @@
 package com.uco.ucopetapi.dto.specialtieDoctor;
 
+import java.util.UUID;
+
 public class SpecialtieDoctorDTO {
+
+    private UUID id;
+    private UUID idDoctor;
+    private UUID idSpecialtie;
+
+
+    public SpecialtieDoctorDTO(){
+    }
+
+    public SpecialtieDoctorDTO(UUID id, UUID idDoctor, UUID idSpecialtie) {
+        this.id = id;
+        this.idDoctor = idDoctor;
+        this.idSpecialtie = idSpecialtie;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getDoctor() {
+        return idDoctor;
+    }
+
+    public void setDoctor(UUID doctor) {
+        this.idDoctor = doctor;
+    }
+
+    public UUID getSpecialtie() {
+        return idSpecialtie;
+    }
+
+    public void setSpecialtie(UUID specialtie) {
+        this.idSpecialtie = specialtie;
+    }
 }
