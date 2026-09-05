771417faeaa15f8b277e062c0201cbcd5cfb6d91
julianamontoya <montoya.julianav@gmail.com>
2026-09-05 11:52:47 -0500

    Codigo Implementación de service, domain y repository para Health PLan y Health Plan coverage

--- src/main/java/com/uco/ucopetapi/domain/healthplancoverage/HealthPlanCoverageDomain.java
+++ src/main/java/com/uco/ucopetapi/domain/healthplancoverage/HealthPlanCoverageDomain.java
@@ -1,4 +1,94 @@
 package com.uco.ucopetapi.domain.healthplancoverage;
 
+import com.uco.ucopetapi.domain.healthplan.HealthPlanDomain;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "health_plan_coverages")
 public class HealthPlanCoverageDomain {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "health_plan_id", nullable = false)
+    private HealthPlanDomain healthPlan;
+
+    //TO DOOOO: Cambiar para obtener el id de procedureDomain
+    @Column(name = "procedure_id", nullable = false)
+    private UUID procedureId;
+
+    @Column(nullable = false)
+    private Integer coveragePercentage;
+
+    @Column(precision = 15, scale = 2)
+    private BigDecimal coverageLimit;
+
+    public HealthPlanCoverageDomain() {
+    }
+
+    public HealthPlanCoverageDomain(
+            UUID id,
+            HealthPlanDomain healthPlan,
+            //TO DO
+            UUID procedureId,
+            Integer coveragePercentage,
+            BigDecimal coverageLimit) {
+
+
+        this.id = id;
+        this.healthPlan = healthPlan;
+        //TO DO
+        this.procedureId = procedureId;
+        this.coveragePercentage = coveragePercentage;
+        this.coverageLimit = coverageLimit;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public HealthPlanDomain getHealthPlan() {
+        return healthPlan;
+    }
+
+    public void setHealthPlan(HealthPlanDomain healthPlan) {
+        this.healthPlan = healthPlan;
+    }
+
+    //TO DO
+    public UUID getProcedureId() {
+        return procedureId;
+    }
+
+    //TO DO
+    public void setProcedureId(UUID serviceId) {
+        this.procedureId = serviceId;
+    }
+
+    public Integer getCoveragePercentage() {
+        return coveragePercentage;
+    }
+
+    public void setCoveragePercentage(Integer coveragePercentage) {
+        this.coveragePercentage = coveragePercentage;
+    }
+
+    public BigDecimal getCoverageLimit() {
+        return coverageLimit;
+    }
+
+    public void setCoverageLimit(BigDecimal coverageLimit) {
+        this.coverageLimit = coverageLimit;
+    }
 }
--- src/main/java/com/uco/ucopetapi/repository/healthplan/IHealthPlanRepository.java
+++ src/main/java/com/uco/ucopetapi/repository/healthplan/IHealthPlanRepository.java
@@ -1,4 +1,13 @@
 package com.uco.ucopetapi.repository.healthplan;
 
-public interface IHealthPlanRepository {
+import com.uco.ucopetapi.domain.healthplan.HealthPlanDomain;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface IHealthPlanRepository extends JpaRepository<HealthPlanDomain, UUID> {
+
+    List<HealthPlanDomain> findByInsuranceCompanyIgnoreCase(String insuranceCompany);
+
 }
