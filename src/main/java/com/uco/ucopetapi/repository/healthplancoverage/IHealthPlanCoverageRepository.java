3c22ac06104d81322963a091c176c5a15707ee27
julianamontoya <montoya.julianav@gmail.com>
2026-09-05 11:52:47 -0500

    Codigo Implementación de service, domain y repository para Health PLan y Health Plan coverage

--- src/main/java/com/uco/ucopetapi/repository/healthplancoverage/IHealthPlanCoverageRepository.java
+++ src/main/java/com/uco/ucopetapi/repository/healthplancoverage/IHealthPlanCoverageRepository.java
@@ -1,4 +1,18 @@
 package com.uco.ucopetapi.repository.healthplancoverage;
 
-public interface IHealthPlanCoverageRepository {
+import com.uco.ucopetapi.domain.healthplancoverage.HealthPlanCoverageDomain;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface IHealthPlanCoverageRepository
+        extends JpaRepository<HealthPlanCoverageDomain, UUID> {
+
+    List<HealthPlanCoverageDomain> findByHealthPlanId(UUID healthPlanId);
+
+    boolean existsByHealthPlanIdAndProcedureId(
+            UUID healthPlanId,
+            UUID serviceId
+    );
 }
