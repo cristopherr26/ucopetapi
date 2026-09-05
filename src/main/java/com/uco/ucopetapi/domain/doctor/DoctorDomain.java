5c47cf8610b9cf94374537eb666b3f6446be3b8b
Clara Rivillas <160364967+crmgg@users.noreply.github.com>
2026-09-05 11:36:08 -0500

    Commit de información de las carpetas de doctor y specialtiedoctor de service, repository y domain

--- src/main/java/com/uco/ucopetapi/domain/doctor/DoctorDomain.java
+++ src/main/java/com/uco/ucopetapi/domain/doctor/DoctorDomain.java
@@ -1,4 +1,59 @@
 package com.uco.ucopetapi.domain.doctor;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "doctors")
 public class DoctorDomain {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "id_person", nullable = false)
+    private UUID idPerson;
+
+    @Column(name = "license_number", nullable = false, unique = true)
+    private String licenseNumber;
+
+
+    public DoctorDomain() {
+    }
+
+    public DoctorDomain(UUID id, UUID idPerson, String licenseNumber) {
+        this.id = id;
+        this.idPerson = idPerson;
+        this.licenseNumber = licenseNumber;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getIdPerson() {
+        return idPerson;
+    }
+
+    public void setIdPerson(UUID idPerson) {
+        this.idPerson = idPerson;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
 }
--- src/main/java/com/uco/ucopetapi/repository/doctor/IDoctorRepository.java
+++ src/main/java/com/uco/ucopetapi/repository/doctor/IDoctorRepository.java
@@ -1,4 +1,14 @@
 package com.uco.ucopetapi.repository.doctor;
 
-public interface IDoctorRepository {
+import com.uco.ucopetapi.domain.doctor.DoctorDomain;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface IDoctorRepository extends JpaRepository<DoctorDomain, UUID> {
+
+    List<DoctorDomain> findByIdPerson(UUID idPerson);
+
+    List<DoctorDomain> findByLicenseNumberContainingIgnoreCase(String licenseNumber);
 }
