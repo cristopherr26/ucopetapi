e56bdd0dee329793af7957961604d68cf4f31f67
nata-1997 <natalia.carmona5843@uco.net.co>
2026-09-10 21:00:55 -0500

    Actualización clases

---
+++ src/main/java/com/uco/ucopetapi/domain/provider/ProviderDomain.java
@@ -0,0 +1,16 @@
+package com.uco.ucopetapi.domain.provider;
+
+import java.util.UUID;
+
+public class ProviderDomain {
+
+    private UUID id;
+    private String providerName;
+    private String representName;
+    private UUID idType;
+    private String documentNumber;
+    private String mobileNumber;
+    private String address;
+    private String email;
+    private boolean isActive;
+}
