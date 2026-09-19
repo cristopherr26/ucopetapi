7301d1c3201c16bb2a5c4a535344508b55bddb6f
Kila005 <maria.diaz7502@uco.net.co>
2026-09-18 20:15:32 -0500

    refactor controller service

--- src/main/java/com/uco/ucopetapi/repository/headquarter/HeadquarterRepository.java
+++ src/main/java/com/uco/ucopetapi/repository/headquarter/HeadquarterRepository.java
@@ -2,8 +2,11 @@ package com.uco.ucopetapi.repository.headquarter;
 
 import com.uco.ucopetapi.domain.headquarter.HeadquarterDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface HeadquarterRepository extends JpaRepository<HeadquarterDomain, UUID> {
+
 }
